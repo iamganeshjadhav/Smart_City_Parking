8bba9c2b4de0f68ced521266dff2b4f7e57c7e17
Ganesh Jadhav <imganeshjadhav965@gmail.com>
2026-01-17 23:31:59 +0530

    add function to handle vehicle entry

---
+++ script.js
@@ -0,0 +1,22 @@
+let parkingList = [];
+
+function addEntry() {
+
+    const vehicleNumber = document.getElementById("vehicleNumber").value;
+    const vehicleType = document.getElementById("vehicleType").value;
+    const zone = document.getElementById("zone").value;
+
+      if (vehicleNumber === "") {   
+        alert("Enter vehicle number");
+        return;
+    }  
+
+    const entry = {                
+        number: vehicleNumber,
+        type: vehicleType,
+        zone: zone,
+        time: new Date().toLocaleString()
+    };  
+
+    parkingList.push(entry);
+}
